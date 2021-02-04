6dc222575ce3d0cc05031bc746e38fae206e7d8b
jeamshiv <58328919+jeamshiv@users.noreply.github.com>
2021-02-04 14:51:51 +0530

    Update main.js

--- codes/main.js
+++ codes/main.js
@@ -1,1 +1,17 @@
-	alert("hi there");
+$(document).ready(function (event) {
+		$("#content4 .entry-body").append(`
+			<!-- AddThis Button BEGIN -->
+		<div class='addthis_toolbox'>
+		<div class='custom_images'>
+		<a class='addthis_button_share'><img alt='Share this Post' border='0' src='http://4.bp.blogspot.com/-7Mh18shJbBw/UR0UQJUeNWI/AAAAAAAADCU/tReZl85IJ9c/s1600/Share+this+Post.png'/></a>
+		<a class='addthis_button_facebook'><img alt='Share to Facebook' border='0' height='30' src='http://1.bp.blogspot.com/-VwUjBqjcw_A/UR0UWOX12aI/AAAAAAAADCk/NMJtDo9H32U/s1600/Facebook+Button+Share.png' width='30'/></a>
+		<a class='addthis_button_twitter'><img alt='Share to Twitter' border='0' height='30' src='http://4.bp.blogspot.com/-HdyrZLb5VQY/UR0UUeHYy5I/AAAAAAAADCc/FaCnOjlaQMQ/s1600/Twitter+Button+Share.png' width='30'/></a>
+		<a class='addthis_button_email'><img alt='Email This' border='0' height='30' src='http://1.bp.blogspot.com/-ct8UEptg4Jo/UR0UXt9d4FI/AAAAAAAADCs/T-qjemKVoT8/s1600/Email+Button+Share.png' width='30'/></a>
+		<a class='addthis_button_pinterest_share'><img alt='Pin This' border='0' height='30' src='http://2.bp.blogspot.com/-J779mbKE8XQ/UR0UYzYDpSI/AAAAAAAADC0/loD-dNI0ky8/s1600/Pinterest+Button+Share.png' width='30'/></a>
+		</div>
+		</div>
+		<script type='text/javascript'>var addthis_config = {"data_track_addressbar":false};</script>
+		<script src='//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-511bbe8405a64ebd' type='text/javascript'/>
+		<!-- AddThis Button END -->
+		`);
+});//ready end
