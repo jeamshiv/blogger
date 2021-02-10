5ec383a0dfc01277ab30d10a20982ec2a410d4cf
jeamshiv <jeamshiv@gmail.com>
2021-02-10 21:44:40 +0530

    test

--- codes/main.js
+++ codes/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function (event) {
 
 
+	$("share-btn-jeamshiv button").click(function (event) {
+		var temp_title = document.title;
+		title = temp_title.replace(/ /g, '+');
+
+		window.location.href=`whatsapp://send?text=window.location.href ${title}`;
+	})
+
 });//ready end
