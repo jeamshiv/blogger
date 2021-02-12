d3823b93a334618fab3848fa4de36f9a278b2d4d
jeamshiv <jeamshiv@gmail.com>
2021-02-12 19:24:19 +0530

    Update main.js

--- codes/main.js
+++ codes/main.js
@@ -6,11 +6,8 @@ $(document).ready(function (event) {
 		var link = window.location.href;
 
 		$(".wp-shareshiv").click(function (event) {
-		if($(window).width()> 768){
-		window.location.href=`https://api.whatsapp.com/send?text=${link} %0a${title}`;} else{window.location.href=`whatsapp://send?text=${link} %0a${titles}`;}
+		window.location.href=`https://api.whatsapp.com/send?text=${link} %0a${title}`;
 		});
-
-
 
 		$(".fb-shareshiv").click(function(event){
 			window.location.href=`https://www.facebook.com/sharer/sharer.php?u=${link}`;
