cc2f603fa107bcfa2f637051f7dafb07813a3b6a
jeamshiv <jeamshiv@gmail.com>
2021-02-14 10:37:16 +0530

    Update main.js

--- codes/main.js
+++ codes/main.js
@@ -1,9 +1,10 @@
-//lazy loading
-document.addEventListener("DOMContentLoaded",function(){var e=[].slice.call(document.querySelectorAll("img.lazy"));if("IntersectionObserver"in window){let n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){let t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}});
-//end
-
 
 $(document).ready(function (event) {
+
+	//lazy loading
+document.addEventListener("DOMContentLoaded",function(){var e=[].slice.call(document.querySelectorAll("img.lazy"));if("IntersectionObserver"in window){let n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){let t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}});
+//end
+	
 
 		//alert("hi there");
 		var temp_title = document.title;
