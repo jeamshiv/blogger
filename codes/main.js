f0aecac9c54c9e61fe8e56bf4e07127feeffd7dc
jeamshiv <jeamshiv@gmail.com>
2021-02-14 10:39:06 +0530

    Update main.js

--- codes/main.js
+++ codes/main.js
@@ -1,11 +1,6 @@
 
 $(document).ready(function (event) {
 
-	//lazy loading
-document.addEventListener("DOMContentLoaded",function(){var e=[].slice.call(document.querySelectorAll("img.lazy"));if("IntersectionObserver"in window){let n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){let t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}});
-//end
-	
-
 		//alert("hi there");
 		var temp_title = document.title;
 		titles = temp_title.replace(/ /g, '+');
