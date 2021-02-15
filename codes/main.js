3d2973e542207c416684e8a52d7ed0649cce12b6
jeamshiv <jeamshiv@gmail.com>
2021-02-15 18:10:30 +0530

    Update main.js

--- codes/main.js
+++ codes/main.js
@@ -1,47 +1,32 @@
+ //<![CDATA[
+var uri = window.location.toString();
+if (uri.indexOf("%3D","%3D") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("%3D"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("%3D%3D","%3D%3D") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("%3D%3D"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("&m=1","&m=1") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("?m=1","?m=1") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+//]]>
+
+ //<![CDATA[
+// Fast Load
+(function(a){a.fn.lazyload=function(b){var c={threshold:0,failurelimit:0,event:"scroll",effect:"show",container:window};if(b){a.extend(c,b)}var d=this;if("scroll"==c.event){a(c.container).bind("scroll",function(b){var e=0;d.each(function(){if(a.abovethetop(this,c)||a.leftofbegin(this,c)){}else if(!a.belowthefold(this,c)&&!a.rightoffold(this,c)){a(this).trigger("appear")}else{if(e++>c.failurelimit){return false}}});var f=a.grep(d,function(a){return!a.loaded});d=a(f)})}this.each(function(){var b=this;if(undefined==a(b).attr("original")){a(b).attr("original",a(b).attr("src"))}if("scroll"!=c.event||undefined==a(b).attr("src")||c.placeholder==a(b).attr("src")||a.abovethetop(b,c)||a.leftofbegin(b,c)||a.belowthefold(b,c)||a.rightoffold(b,c)){if(c.placeholder){a(b).attr("src",c.placeholder)}else{a(b).removeAttr("src")}b.loaded=false}else{b.loaded=true}a(b).one("appear",function(){if(!this.loaded){a("<img />").bind("load",function(){a(b).hide().attr("src",a(b).attr("original"))[c.effect](c.effectspeed);b.loaded=true}).attr("src",a(b).attr("original"))}});if("scroll"!=c.event){a(b).bind(c.event,function(c){if(!b.loaded){a(b).trigger("appear")}})}});a(c.container).trigger(c.event);return this};a.belowthefold=function(b,c){if(c.container===undefined||c.container===window){var d=a(window).height()+a(window).scrollTop()}else{var d=a(c.container).offset().top+a(c.container).height()}return d<=a(b).offset().top-c.threshold};a.rightoffold=function(b,c){if(c.container===undefined||c.container===window){var d=a(window).width()+a(window).scrollLeft()}else{var d=a(c.container).offset().left+a(c.container).width()}return d<=a(b).offset().left-c.threshold};a.abovethetop=function(b,c){if(c.container===undefined||c.container===window){var d=a(window).scrollTop()}else{var d=a(c.container).offset().top}return d>=a(b).offset().top+c.threshold+a(b).height()};a.leftofbegin=function(b,c){if(c.container===undefined||c.container===window){var d=a(window).scrollLeft()}else{var d=a(c.container).offset().left}return d>=a(b).offset().left+c.threshold+a(b).width()};a.extend(a.expr[":"],{"below-the-fold":"$.belowthefold(a, {threshold : 0, container: window})","above-the-fold":"!$.belowthefold(a, {threshold : 0, container: window})","right-of-fold":"$.rightoffold(a, {threshold : 0, container: window})","left-of-fold":"!$.rightoffold(a, {threshold : 0, container: window})"})})(jQuery);$(function(){$("img").lazyload({placeholder:"https://1.bp.blogspot.com/-Qg5bi1ZtDdM/VZ5nHAyYBqI/AAAAAAAAChE/exGnasO4oyk/s640/arlinadesign.gif",effect:"fadeIn",threshold:"-50"})}); //]]>
+
 
 $(document).ready(function (event) {
-
-		const typedTextSpan = document.querySelector(".typed-text");
-		const cursorSpan = document.querySelector(".cursor");
-
-		const textArray = ["hard", "fun", "a journey", "LIFE"];
-		const typingDelay = 200;
-		const erasingDelay = 100;
-		const newTextDelay = 2000; // Delay between current and next text
-		let textArrayIndex = 0;
-		let charIndex = 0;
-
-		function type() {
-		  if (charIndex < textArray[textArrayIndex].length) {
-		    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-		    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-		    charIndex++;
-		    setTimeout(type, typingDelay);
-		  } 
-		  else {
-		    cursorSpan.classList.remove("typing");
-		  	setTimeout(erase, newTextDelay);
-		  }
-		}
-
-		function erase() {
-			if (charIndex > 0) {
-		    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-		    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-		    charIndex--;
-		    setTimeout(erase, erasingDelay);
-		  } 
-		  else {
-		    cursorSpan.classList.remove("typing");
-		    textArrayIndex++;
-		    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-		    setTimeout(type, typingDelay + 1100);
-		  }
-		}
-
-		document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-		  if(textArray.length) setTimeout(type, newTextDelay + 250);
-		});
 
 		//alert("hi there");
 		var temp_title = document.title;
